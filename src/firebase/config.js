0fbbd5230f039af49ace2819144aa83a332432da
abhishekmishr04 <abhishek945637@gmail.com>
2025-12-28 01:28:17 +0530

    add:code

---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAeZu7wKyvMv15bo_MR7XSeEPGxdVs3FCs",
+    authDomain: "thecollabsite.firebaseapp.com",
+    projectId: "thecollabsite",
+    storageBucket: "thecollabsite.appspot.com",
+    messagingSenderId: "1079156643343",
+    appId: "1:1079156643343:web:165e426947d06b439ff57a"
+}
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+
+//init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, projectStorage, timestamp }
